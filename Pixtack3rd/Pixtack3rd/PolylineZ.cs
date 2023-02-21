4f251d584fe59884054eb6c3b05078d2d69eb362
waten <gogowaten@yahoo.co.jp>
2023-02-21 22:09:57 +0900

    202302212209

---
+++ Pixtack3rd/Pixtack3rd/PolylineZ.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Shapes;
+
+//WPF Arrow and Custom Shapes - CodeProject
+//https://www.codeproject.com/Articles/23116/WPF-Arrow-and-Custom-Shapes
+
+//2022WPF/Arrow.cs at master · gogowaten/2022WPF
+//https://github.com/gogowaten/2022WPF/blob/master/20221203_%E7%9F%A2%E5%8D%B0%E5%9B%B3%E5%BD%A2/20221203_%E7%9F%A2%E5%8D%B0%E5%9B%B3%E5%BD%A2/Arrow.cs
+
+//E:\オレ\エクセル\WPFでPixtack紫陽花.xlsm_三角関数_$B$95
+
+//WPFで矢印直線描画、Shapeクラスを継承して作成してみた - 午後わてんのブログ
+//https://gogowaten.hatenablog.com/entry/2023/02/20/162212
+
+namespace Pixtack3rd
+{
+    public enum HeadType { None = 0, Arrow, }
+    public class PolylineZ : Shape
+    {
+        #region 依存プロパティ
+
+        /// <summary>
+        /// 終点のヘッドタイプ
+        /// </summary>
+        public HeadType HeadEndType
+        {
+            get { return (HeadType)GetValue(HeadEndTypeProperty); }
+            set { SetValue(HeadEndTypeProperty, value); }
+        }
+        public static readonly DependencyProperty HeadEndTypeProperty =
+            DependencyProperty.Register(nameof(HeadEndType), typeof(HeadType), typeof(PolylineZ),
+                new FrameworkPropertyMetadata(HeadType.None,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure));
+        /// <summary>
+        /// 始点のヘッドタイプ
+        /// </summary>
+        public HeadType HeadBeginType
+        {
+            get { return (HeadType)GetValue(HeadBeginTypeProperty); }
+            set { SetValue(HeadBeginTypeProperty, value); }
+        }
+        public static readonly DependencyProperty HeadBeginTypeProperty =
+            DependencyProperty.Register(nameof(HeadBeginType), typeof(HeadType), typeof(PolylineZ),
+                new FrameworkPropertyMetadata(HeadType.None,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        /// <summary>
+        /// 矢印角度、初期値は30.0にしている。30～40くらいが適当
+        /// </summary>
+        public double Angle
+        {
+            get { return (double)GetValue(AngleProperty); }
+            set { SetValue(AngleProperty, value); }
+        }
+        public static readonly DependencyProperty AngleProperty =
+            DependencyProperty.Register(nameof(Angle), typeof(double), typeof(PolylineZ),
+                new FrameworkPropertyMetadata(30.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        //[TypeConverter(typeof(MyTypeConverterPoints))]
+        //public ObservableCollection<Point> MyPoints
+        //{
+        //    get { return (ObservableCollection<Point>)GetValue(MyPointsProperty); }
+        //    set { SetValue(MyPointsProperty, value); }
+        //}
+        //public static readonly DependencyProperty MyPointsProperty =
+        //    DependencyProperty.Register(nameof(MyPoints), typeof(ObservableCollection<Point>), typeof(PolylineZ),
+        //        new FrameworkPropertyMetadata(new ObservableCollection<Point>() { new Point(0, 0), new Point(100, 100) },
+        //            FrameworkPropertyMetadataOptions.AffectsRender |
+        //            FrameworkPropertyMetadataOptions.AffectsMeasure));
+        public PointCollection MyPoints
+        {
+            get { return (PointCollection)GetValue(MyPointsProperty); }
+            set { SetValue(MyPointsProperty, value); }
+        }
+
+        public static readonly DependencyProperty MyPointsProperty =
+            DependencyProperty.Register(nameof(MyPoints), typeof(PointCollection), typeof(PolylineZ),
+                new FrameworkPropertyMetadata(new PointCollection() { new Point(0, 0), new Point(100, 100) },
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        #endregion 依存プロパティ
+
+        private static double DegreeToRadian(double degree)
+        {
+            return degree / 360.0 * (Math.PI * 2.0);
+        }
+
+        /// <summary>
+        /// 直線の描画
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="begin"></param>
+        /// <param name="end"></param>
+        private void DrawLine(StreamGeometryContext context, Point begin, Point end)
+        {
+            context.BeginFigure(begin, false, false);
+            for (int i = 1; i < MyPoints.Count - 1; i++)
+            {
+                context.LineTo(MyPoints[i], true, false);
+            }
+            context.LineTo(end, true, false);
+        }
+
+        protected override Geometry DefiningGeometry
+        {
+            get
+            {
+                StreamGeometry geometry = new() { FillRule = FillRule.Nonzero };
+                using (var context = geometry.Open())
+                {
+                    Point begin = MyPoints[0];
+                    Point end = MyPoints[^1];
+                    switch (HeadBeginType)
+                    {
+                        case HeadType.None:
+                            break;
+                        case HeadType.Arrow:
+                            begin = DrawBeginArrow(context);
+                            break;
+                    }
+                    switch (HeadEndType)
+                    {
+                        case HeadType.None:
+                            break;
+                        case HeadType.Arrow:
+                            end = DrawEndArrow(context);
+                            break;
+                    }
+                    DrawLine(context, begin, end);
+                }
+                geometry.Freeze();
+                return geometry;
+            }
+        }
+
+        /// <summary>
+        /// 始点にアローヘッド描画
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns>アローヘッドと直線との接点座標</returns>
+        private Point DrawBeginArrow(StreamGeometryContext context)
+        {
+            double x0 = MyPoints[0].X;
+            double y0 = MyPoints[0].Y;
+            double x1 = MyPoints[1].X;
+            double y1 = MyPoints[1].Y;
+
+            double lineRadian = Math.Atan2(y1 - y0, x1 - x0);
+            double arrowRadian = DegreeToRadian(Angle);
+            double headSize = StrokeThickness * 2.0;
+            double wingLength = headSize / Math.Cos(arrowRadian);
+
+            double wingRadian1 = lineRadian + arrowRadian;
+            Point arrowP1 = new(
+                wingLength * Math.Cos(wingRadian1) + x0,
+                wingLength * Math.Sin(wingRadian1) + y0);
+            double wingRadian2 = lineRadian - DegreeToRadian(Angle);
+            Point arrowP2 = new(
+                wingLength * Math.Cos(wingRadian2) + x0,
+                wingLength * Math.Sin(wingRadian2) + y0);
+            //アローヘッド描画、Fill(塗りつぶし)で描画
+            context.BeginFigure(MyPoints[0], true, false);//fill, close
+            context.LineTo(arrowP1, false, false);//isStroke, isSmoothJoin
+            context.LineTo(arrowP2, false, false);
+
+            //アローヘッドと直線との接点座標、
+            //HeadSizeぴったりで計算すると僅かな隙間ができるので-1.0している、
+            //-0.5でも隙間になる、-0.7で隙間なくなる
+            return new Point(
+                (headSize - 1.0) * Math.Cos(lineRadian) + x0,
+                (headSize - 1.0) * Math.Sin(lineRadian) + y0);
+
+        }
+
+        /// <summary>
+        /// 終点にアローヘッド描画
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns>アローヘッドと直線との接点座標</returns>
+        private Point DrawEndArrow(StreamGeometryContext context)
+        {
+            double x0 = MyPoints[^1].X;
+            double x1 = MyPoints[^2].X;
+            double y0 = MyPoints[^1].Y;
+            double y1 = MyPoints[^2].Y;
+
+            double lineRadian = Math.Atan2(y1 - y0, x1 - x0);
+            double arrowRadian = DegreeToRadian(Angle);
+            double headSize = StrokeThickness * 2.0;
+            double wingLength = headSize / Math.Cos(arrowRadian);
+
+            double wingRadian1 = lineRadian + arrowRadian;
+            Point arrowP1 = new(
+                wingLength * Math.Cos(wingRadian1) + x0,
+                wingLength * Math.Sin(wingRadian1) + y0);
+            double wingRadian2 = lineRadian - DegreeToRadian(Angle);
+            Point arrowP2 = new(
+                wingLength * Math.Cos(wingRadian2) + x0,
+                wingLength * Math.Sin(wingRadian2) + y0);
+            //アローヘッド描画、Fill(塗りつぶし)で描画
+            context.BeginFigure(MyPoints[^1], true, false);//fill, close
+            context.LineTo(arrowP1, false, false);//isStroke, isSmoothJoin
+            context.LineTo(arrowP2, false, false);
+
+            return new Point(
+                (headSize - 1.0) * Math.Cos(lineRadian) + x0,
+                (headSize - 1.0) * Math.Sin(lineRadian) + y0);
+        }
+
+    }
+}
--- Pixtack3rd/Pixtack3rd/TThumb.cs
+++ Pixtack3rd/Pixtack3rd/TThumb.cs
@@ -20,6 +20,7 @@ using System.Collections.Specialized;
 using System.Windows.Shapes;
 using System.IO;
 using System.Dynamic;
+using System.Xml.Linq;
 
 namespace Pixtack3rd
 {
@@ -2346,6 +2347,161 @@ namespace Pixtack3rd
 
     }
 
+    public class TTPolylineZ : TThumb
+    {
+          #region 依存プロパティ
+
+        /// <summary>
+        /// 終点のヘッドタイプ
+        /// </summary>
+        public HeadType HeadEndType
+        {
+            get { return (HeadType)GetValue(HeadEndTypeProperty); }
+            set { SetValue(HeadEndTypeProperty, value); }
+        }
+        public static readonly DependencyProperty HeadEndTypeProperty =
+            DependencyProperty.Register(nameof(HeadEndType), typeof(HeadType), typeof(TTPolylineZ),
+                new FrameworkPropertyMetadata(HeadType.None,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        /// <summary>
+        /// 始点のヘッドタイプ
+        /// </summary>
+        public HeadType HeadBeginType
+        {
+            get { return (HeadType)GetValue(HeadBeginTypeProperty); }
+            set { SetValue(HeadBeginTypeProperty, value); }
+        }
+        public static readonly DependencyProperty HeadBeginTypeProperty =
+            DependencyProperty.Register(nameof(HeadBeginType), typeof(HeadType), typeof(TTPolylineZ),
+                new FrameworkPropertyMetadata(HeadType.None,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public double HeadAngle
+        {
+            get { return (double)GetValue(HeadAngleProperty); }
+            set { SetValue(HeadAngleProperty, value); }
+        }
+        public static readonly DependencyProperty HeadAngleProperty =
+            DependencyProperty.Register(nameof(HeadAngle), typeof(double), typeof(TTPolylineZ),
+                new FrameworkPropertyMetadata(30.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public PointCollection MyPoints
+        {
+            get { return (PointCollection)GetValue(MyPointsProperty); }
+            set { SetValue(MyPointsProperty, value); }
+        }
+        public static readonly DependencyProperty MyPointsProperty =
+            DependencyProperty.Register(nameof(MyPoints), typeof(PointCollection), typeof(TTPolylineZ),
+                new FrameworkPropertyMetadata(new PointCollection() { new Point(0, 0), new Point(100, 100) },
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public Brush Stroke
+        {
+            get { return (Brush)GetValue(StrokeProperty); }
+            set { SetValue(StrokeProperty, value); }
+        }
+        public static readonly DependencyProperty StrokeProperty =
+            DependencyProperty.Register(nameof(Stroke), typeof(Brush), typeof(TTPolylineZ),
+                new FrameworkPropertyMetadata(Brushes.Red,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public Brush TTFill
+        {
+            get { return (Brush)GetValue(TTFillProperty); }
+            set { SetValue(TTFillProperty, value); }
+        }
+        public static readonly DependencyProperty TTFillProperty =
+            DependencyProperty.Register(nameof(TTFill), typeof(Brush), typeof(TTPolylineZ),
+                new FrameworkPropertyMetadata(Brushes.Red,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public double StrokeThickness
+        {
+            get { return (double)GetValue(StrokeThicknessProperty); }
+            set { SetValue(StrokeThicknessProperty, value); }
+        }
+        public static readonly DependencyProperty StrokeThicknessProperty =
+            DependencyProperty.Register(nameof(StrokeThickness), typeof(double), typeof(TTPolylineZ),
+                new FrameworkPropertyMetadata(5.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+
+
+        #endregion 依存プロパティ
+
+        private readonly PolylineZ MyTemplateElement;
+
+
+        public TTPolylineZ() : this(new Data(TType.Polyline)) { }
+        public TTPolylineZ(Data data) : base(data)
+        {
+            Data = data;
+            this.DataContext = Data;
+            if (MakeTemplate<PolylineZ>() is PolylineZ element) { MyTemplateElement = element; }
+            else { throw new ArgumentException("テンプレート作成できんかった"); }
+
+            MySetBinding();
+        }
+        private void MySetBinding()
+        {
+            //Points同士の連携はBindingより=(イコール)でしたほうが楽
+            //連携の方向は
+            //mydata <- this <- polyz、で右側がSource
+            //mydata <- this ここをイコール
+            //this < -polyz ここはBinding
+            Loaded += (a, b) => { Data.PointCollection = MyPoints; };
+            this.SetBinding(MyPointsProperty, new Binding() { Source = MyTemplateElement, Path = new PropertyPath(PolylineZ.MyPointsProperty) });
+
+            ////以下だと値の更新はされるけど、見た目の更新がされない。
+            ////→値更新後にPolylineZに対してInvalidateVisual();を実行したら見た目も更新された
+            ////でもめんどくさいから上の方法が良い
+            //MyPolylineZ.SetBinding(PolylineZ.MyPointsProperty,new Binding() { Source=this,Path= new PropertyPath(MyPointsProperty) });
+            //this.SetBinding(MyPointsProperty,new Binding(nameof(MyData.PointCollection)) { Source=this.MyData });
+
+            //Bindingの方向、Dataの各プロパティは依存プロパティではないのでTargetにはできないので
+            //mydata <- this や mydata <- polyz とかにはできない
+            //なのでできるのは以下の3種類
+            //polyz <- this & this <- mydata 今回はこれ
+            //this <- polyz & polyz <- mydata
+            //this <- mydata & polyz <- mydata
+
+
+            //polyz <- this
+            MyTemplateElement.DataContext = this;
+            MyTemplateElement.SetBinding(Shape.StrokeThicknessProperty, nameof(StrokeThickness));
+            MyTemplateElement.SetBinding(Shape.StrokeProperty, nameof(Stroke));
+            MyTemplateElement.SetBinding(Shape.FillProperty, nameof(TTFill));
+            MyTemplateElement.SetBinding(PolylineZ.HeadBeginTypeProperty, nameof(HeadBeginType));
+            MyTemplateElement.SetBinding(PolylineZ.HeadEndTypeProperty, nameof(HeadEndType));
+            MyTemplateElement.SetBinding(PolylineZ.AngleProperty, nameof(HeadAngle));
+
+            //this <- mydata
+            this.DataContext = this.Data;
+            this.SetBinding(StrokeThicknessProperty, nameof(Data.StrokeThickness));
+            this.SetBinding(StrokeProperty, nameof(Data.Stroke));
+            this.SetBinding(TTFillProperty, nameof(Data.Fill));
+            this.SetBinding(HeadEndTypeProperty, nameof(Data.HeadEndType));
+            this.SetBinding(HeadBeginTypeProperty, nameof(Data.HeadBeginType));
+            this.SetBinding(HeadAngleProperty, nameof(Data.HeadAngle));
+        }
+    }
+
+    
     public class TTImage : TThumb
     {
         //画像ファイルのフルパス、変更時にコールバックでBitmapSourceを作成して表示する
@@ -2382,6 +2538,7 @@ namespace Pixtack3rd
         }
 
     }
+
 
 
 
